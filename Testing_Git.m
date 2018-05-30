c1a920187182829ee8ef78002b098eeed58cd4e2
johnthompson <johnthompson@johns-mbp>
2018-05-30 08:07:12 -0600

    trying to get all files on the github

--- Testing_Git.m
+++
@@ -1,9 +0,0 @@
-%This is a test of the github thing...
-
-disp('This is a test...')
-
-%I am modifying the file
-disp('This is a modification to the file')
-
-%post-commit modification to file...
-disp('This is a modification to the file after it was "committed"')
